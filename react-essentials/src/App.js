c5184cee90886524d107a42409dfd45abd2dbc74
madhav06 <mnandan06@gmail.com>
2021-02-06 00:57:01 +0530

    adding react components: Header, Main, Footer

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header> 
+      <h1> The Biryani Mahal</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <section> 
+      <p>We serve the most delicious biryani in varanasi city.</p>
+      <p> </p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer> 
+      <p> Working Hours: 9:00 AM to 8:00 PM </p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main />
+      <Footer />
     </div>
   );
 }
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 
 ReactDOM.render(
-  <ul> 
-    <li> Monday </li>
-    <li> Tuesday </li>
-    <li> Wednesday </li>
-    <li> Thursday </li>
-    <li> Friday </li>
-    <li> Saturday </li>
-
-  </ul>,
+  <App />,
   document.getElementById('root')
 );
 
