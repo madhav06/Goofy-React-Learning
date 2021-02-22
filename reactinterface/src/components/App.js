2e5930854aafd3ec5f9be724934e0f9065920124
madhav06 <mnandan06@gmail.com>
2021-02-22 13:47:41 +0530

    + AddAppointments form

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -1,53 +1,67 @@
-import { result } from 'lodash';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
-import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+import { without } from 'lodash';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       myAppointments: [],
+      formDisplay: false,
       lastIndex: 0
     };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
   }
 
   componentDidMount() {
-    fetch('./data.json').then(response => response.json()).then(result => {
-      const apts = result.map(item => {
-        item.aptId = this.state.lastIndex;
-        this.setState({ lastIndex: this.state.lastIndex + 1})
-        return item;
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
       });
-      this.setState({
-        myAppointments: apts
-      });
-    });
   }
-  render() {
-    
 
-    return(
+  render() {
+    return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                
-                < AddAppointments />
-                < SearchAppointments />
-                < ListAppointments appointments={this.state.myAppointments} />
+                <AddAppointments formDisplay={this.state.formDisplay} />
+                <SearchAppointments />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}
+                />
               </div>
             </div>
           </div>
         </div>
       </main>
     );
-
   }
 }
-    
+
 export default App;
--- reactinterface/src/components/ListAppointments.js
+++ reactinterface/src/components/ListAppointments.js
@@ -9,13 +9,23 @@ class ListAppointments extends Component {
         {this.props.appointments.map(item => (
           <div className="pet-item col media py-3" key={item.aptId}>
             <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger"><FaTimes /></button>
+              <button
+                className="pet-delete btn btn-sm btn-danger"
+                onClick={() => this.props.deleteAppointment(item)}>
+                <FaTimes />
+              </button>
             </div>
 
             <div className="pet-info media-body">
               <div className="pet-head d-flex">
-                <span className="pet-name">{item.aptId}--{item.petName}</span>
-                <span className="apt-date ml-auto"><Moment date={item.aptDate} parse="YYYY-MM-dd hh:mm" format="MMM-D h:mma" /></span>
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">
+                  <Moment
+                    date={item.aptDate}
+                    parse="YYYY-MM-dd hh:mm"
+                    format="MMM-D h:mma"
+                  />
+                </span>
               </div>
 
               <div className="owner-name">
