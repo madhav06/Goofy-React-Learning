9d4e8c6bf6f7fe7bfb0a01ba2d74d1822c15103b
madhav06 <mnandan06@gmail.com>
2021-02-22 20:51:05 +0530

    handling a toggle

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -16,6 +16,11 @@ class App extends Component {
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({formDisplay: !this.setState.formDisplay});
   }
 
   deleteAppointment(apt) {
@@ -49,7 +54,7 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments formDisplay={this.state.formDisplay} />
+                <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} />
                 <SearchAppointments />
                 <ListAppointments
                   appointments={this.state.myAppointments}
