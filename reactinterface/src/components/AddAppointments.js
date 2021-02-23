eb17a4984f7bf07959bf11640c645319a1ec8c1e
madhav06 <mnandan06@gmail.com>
2021-02-23 14:52:50 +0530

    + making searchbar for appointments

--- reactinterface/src/components/AddAppointments.js
+++ reactinterface/src/components/AddAppointments.js
@@ -2,6 +2,51 @@ import React, { Component } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 class AddAppointments extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            petName: '',
+            ownerName: '',
+            aptDate: '',
+            aptTime: '',
+            aptNotes: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd(e) {
+        e.preventDefault();
+        let tempApt = {
+            petName: this.state.petName,
+            ownerName: this.state.ownerName,
+            aptDate: this.state.aptDate + ' ' + this.state.aptTime,
+            aptNotes: this.state.aptNotes
+        };
+        this.props.AddAppointment(tempApt);
+
+        this.setState({
+            petName: '',
+            ownerName: '',
+            aptDate: '',
+            aptTime: '',
+            aptNotes: ''
+        });
+        this.props.toggleForm();
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+  
   render() {
     return (
       <div
@@ -15,7 +60,7 @@ class AddAppointments extends Component {
         </div>
 
         <div className="card-body">
-          <form id="aptForm" noValidate>
+          <form id="aptForm" noValidate onSubmit={this.handleAdd}>
             <div className="form-group form-row">
               <label
                 className="col-md-2 col-form-label text-md-right"
@@ -30,6 +75,8 @@ class AddAppointments extends Component {
                   className="form-control"
                   name="petName"
                   placeholder="Pet's Name"
+                  value={this.state.petName}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
@@ -47,6 +94,8 @@ class AddAppointments extends Component {
                   className="form-control"
                   name="ownerName"
                   placeholder="Owner's Name"
+                  value={this.state.ownerName}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
@@ -64,6 +113,8 @@ class AddAppointments extends Component {
                   className="form-control"
                   name="aptDate"
                   id="aptDate"
+                  value={this.state.aptDate}
+                  onChange={this.handleChange}
                 />
               </div>
               <label
@@ -78,6 +129,8 @@ class AddAppointments extends Component {
                   className="form-control"
                   name="aptTime"
                   id="aptTime"
+                  value={this.state.aptTime}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
@@ -94,6 +147,8 @@ class AddAppointments extends Component {
                   name="aptNotes"
                   id="aptNotes"
                   placeholder="Appointment Notes"
+                  value={this.state.aptNotes}
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { findIndex, without } from 'lodash';
 
 class App extends Component {
   constructor() {
@@ -13,14 +13,55 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
+    this.AddAppointment = this.AddAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+  }
+
+  searchApts(query) {
+    this.setState({queryText: query});
+  }
+
+
+
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  updateInfo(name, value, id){
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    })
   }
 
   toggleForm() {
     this.setState({formDisplay: !this.setState.formDisplay});
+  }
+
+  AddAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
   }
 
   deleteAppointment(apt) {
@@ -48,16 +89,41 @@ class App extends Component {
   }
 
   render() {
+    let order;
+    let filterApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc'){
+      order = 1;
+    }
+    else {
+      order = -1;
+    }
+
+    filterApts = filterApts.sort((a,b) => {
+      if(a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase()) {
+        return -1*order;
+      } else {
+        return 1*order;
+      }
+    }).filter(eachItem => {
+      return (
+        eachItem['petName'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+
+        eachItem['ownerName'].toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+
+        eachItem['aptNotes'].toLowerCase().includes(this.state.queryText.toLowerCase())
+      );
+    });
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} />
-                <SearchAppointments />
+                <AddAppointments formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} AddAppointment={this.AddAppointment} />
+                <SearchAppointments orderBy={this.state.orderBy} orderDir={this.state.orderDir} changeOrder={this.changeOrder} searchApts={this.searchApts}/>
                 <ListAppointments
-                  appointments={this.state.myAppointments}
+                  appointments={filterApts}
                   deleteAppointment={this.deleteAppointment}
                 />
               </div>
