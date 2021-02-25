d7bc69ad1c9b852374a125006597707a79067d95
madhav06 <mnandan06@gmail.com>
2021-02-25 17:04:05 +0530

    +adding a dialogbox for Post Job

--- react-indeed/src/components/Job/NewJobModal.js
+++ react-indeed/src/components/Job/NewJobModal.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogActions, DialogContent } from '@material-ui/core';
+import { Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogActions, DialogContent, makeStyles, Typography, Button, IconButton } from '@material-ui/core';
+import {Close as CloseIcon} from "@material-ui/icons";
 
+
+const useStyles = makeStyles((theme) => ({
+    skillsChip: {
+        margin: theme.spacing(0.5),
+        padding: theme.spacing(0.75),
+        fontSize: "14.5px",
+        borderRadius: "5px",
+        fontWeight: 600,
+        border: `1px solid ${theme.palette.secondary.main}`,
+        color: theme.palette.secondary.main,
+        cursor: "pointer",
+
+        "&:hover": {
+            backgroundColor: theme.palette.secondary.main,
+            color: "#fff",
+        },
+    },
+}));
 
 export default (props) => {
+    const classes = useStyles();
+    const skills = ["React","Python","DjangoORM","Node", "NoSql", "Sql", "AWS", "Vue","Git"];
     return(
         <Dialog open={true} fullWidth>
-            <DialogTitle>Post Job</DialogTitle>
+            <DialogTitle>
+                <Box display="flex" justifyContent="space-betwwen" alignItems="center">
+                    Post Job
+                    <IconButton>
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+            </DialogTitle>
             <DialogContent>
                 <Grid container spacing={2}>
                     <Grid item xs={6}>
@@ -37,7 +65,21 @@ export default (props) => {
                       <FilledInput placeholder="Job description*" disableUnderline fullWidth multiline rows={5}/>
                     </Grid>
                 </Grid>
+                <Box mt={2}>
+                    <Typography>Skills:</Typography>
+                    <Box display="flex">
+                        {skills.map((skill) => (
+                            <Box className={classes.skillsChip} key={skill}>{skill}</Box>
+                        ))}
+                    </Box>
+                </Box>
             </DialogContent>
+            <DialogActions>
+                <Box color="red" alignItems="center" display="flex" width="100%" justifyContent="space-between">
+                    <Typography variant="caption">*Required fields</Typography>
+                    <Button variant="contained" disableElevation color="primary">Post Job</Button>
+                </Box>
+            </DialogActions>
         </Dialog>
     );
 };
