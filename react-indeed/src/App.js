091bed1be4868051ed29141453a522d3fd021275
madhav06 <mnandan06@gmail.com>
2021-02-26 19:54:27 +0530

    +adding loading CircularProgressBar

--- react-indeed/src/App.js
+++ react-indeed/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Box, Grid, ThemeProvider} from "@material-ui/core";
+import {Box, Grid, ThemeProvider, Typography, CircularProgress} from "@material-ui/core";
 import theme from './theme/theme';
 import Header from "./components/Header/";
 import SearchBar from './components/SearchBar';
@@ -10,12 +10,14 @@ import {firestore} from './firebase/config';
 
 export default () => {
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchJobs = async() => {
     const req = await firestore.collection('jobs').orderBy('location', 'desc').get();
     const tempJobs = req.docs.map((job) => ({...job.data(), id: job.id,}));
     console.log(tempJobs);
     setJobs(tempJobs);
+    setLoading(false);
   };
   useEffect(() => {
     fetchJobs();
@@ -28,9 +30,9 @@ export default () => {
     <Grid container justify="center"> 
       <Grid item xs={10}>
         <SearchBar />
-        {jobs.map((job) => (
+        {loading ? (<Box display="flex" justifyContent="center" ><CircularProgress />  </Box>): (jobs.map((job) => (
           <JobCard key={job.id} {...job} />
-        ))}
+        )))}
       </Grid>
     </Grid>
   </ThemeProvider>
